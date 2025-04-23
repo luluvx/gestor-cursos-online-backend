8b30063a3a94ebeea3c951631f3435098ba2d3f8
camila-luksic <83142600+camila-luksic@users.noreply.github.com>
2025-04-23 12:54:33 -0400

    Implementar endpoint para crear un rol POST /roles

--- routes/rol.routes.js
+++ routes/rol.routes.js
@@ -1,6 +1,8 @@
 module.exports = app => {
     let router = require("express").Router();
     const controller = require("../controllers/rol.controller.js");
+    
+    router.post("/",controller.createRol);
 
 
 
