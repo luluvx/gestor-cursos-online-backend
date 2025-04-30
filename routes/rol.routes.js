79e4104c47da1114b6ccb8b211c1adeaac6c258a
Helen Baldelomar <helendiyhana03@gmail.com>
2025-04-30 07:35:28 -0400

    Get

--- routes/rol.routes.js
+++ routes/rol.routes.js
@@ -3,6 +3,9 @@ module.exports = app => {
     const controller = require("../controllers/rol.controller.js");
     
     router.post("/",controller.createRol);
+    router.get("/",controller.listRol);
+
+    router.get("/:id",controller.getRolById);
 
 
 
